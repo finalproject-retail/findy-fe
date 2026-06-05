489ec387756d1a2de84519616244fad8dfbff9b2
chaelyn-kim <chaelyn.kim1125@gmail.com>
2026-06-05 12:48:14 +0900

    feat: 챗봇 스크린 디자인 수정

--- findy-fe/app/chatbot.tsx
+++ findy-fe/app/chatbot.tsx
@@ -1,11 +1,20 @@
 import { ChatbotScreen } from "@/components/chatbot";
 import { Header } from "@/components/common";
 import { SafeView } from "@/components/layout";
+import { View } from "react-native";
 
 export default function ChatbotRoute() {
   return (
     <SafeView>
       <Header showBack showCenterLogo />
+
+      <View
+        style={{
+          height: 1,
+          backgroundColor: "#E5E7EB",
+        }}
+      />
+
       <ChatbotScreen />
     </SafeView>
   );
